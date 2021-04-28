07d4d3452a6f1fb3f5739f7cb86ef68e39ca52f3
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-28 21:46:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Evolution simulator
+@author: Anatole Hernot
+@version: 0.0.1
+"""
+
+import sys
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) == 3:
+        print(sys.argv[1], sys.argv[2])
